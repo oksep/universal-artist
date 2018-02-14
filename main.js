3ce510aed8ebe61c7b4e689f926492af5a1cb6f6
RenYufeng <seven__up@sina.cn>
2018-02-14 21:27:03 +0800

    update

--- main.js
+++ main.js
@@ -1,35 +1,49 @@
-const {app, BrowserWindow} = require("electron");
+const electron = require('electron');
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
 
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    backgroundColor: '#FFFFFF',
-    icon: `file://${__dirname}/dist/asset/logo.png`
-  });
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        backgroundColor: '#FFFFFF',
+        icon: `file://${__dirname}/dist/asset/logo.png`,
+        // minWidth: 800,
+        // minHeight: 600
+    });
 
-  // win.loadURL(`file://${__dirname}/dist/index.html`);
-  win.loadURL('http://localhost:4200/');
+    // win.loadURL(url.format({
+    //     pathname: path.join(__dirname, 'build/index.html'),
+    //     protocol: 'file:',
+    //     slashes: true
+    // }));
 
-  win.webContents.openDevTools();
+    win.loadURL('http://localhost:4200/');
 
-  win.on('closed', function () {
-    win = null
-  });
+    // win.webContents.openDevTools();
+
+    win.on('closed', function () {
+        win = null
+    });
 }
 
 app.on('ready', createWindow);
 
 app.on('all-window-closed', function () {
-  if (app.platform !== 'darwin') {
-    app.quit()
-  }
+    if (app.platform !== 'darwin') {
+        app.quit()
+    }
 });
 
 app.on('active', function () {
-  if (win == null) {
-    createWindow()
-  }
+    if (win == null) {
+        createWindow()
+    }
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ElectronService} from 'ngx-electron';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+	constructor(private electronService: ElectronService) {
+	}
 }
